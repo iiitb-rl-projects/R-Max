8835c5cf863394e87cbbfc8bd9a817e1ca8593d9
jaishankargm015 <36201305+jaishankargm015@users.noreply.github.com>
2018-02-07 17:35:03 +0530

    Update My_Environment.py
    Added some documentation.

--- Taxi Env/My_Environment.py
+++ Taxi Env/My_Environment.py
@@ -7,14 +7,18 @@ from rlglue.types import Action
 from rlglue.types import Reward_observation_terminal
 from gym.envs.toy_text import taxi
 
+#My_Environment is inheriting the class TaxiEnv from gym/envs/toy_text/taxi.py
+
 class My_Environment(taxi.TaxiEnv):
 	# () -> string
 	def env_init(self):
+		#initializes the taxi environment map and states and rewards and initial state distribution.
 		taxi.TaxiEnv.__init__(self);
 		return "VERSION RL-Glue-3.0 PROBLEMTYPE episodic DISCOUNTFACTOR 1 OBSERVATIONS INTS (0 499) ACTIONS INTS (0 5) REWARDS (-10.0 20.0) EXTRA TaxiEnv by Us."
 	
 	# () -> Observation
  	def env_start(self):
+		#seed and reset are defined in gym/envs/toy_text/discrete.py which is included in taxi.py chooses a fresh start state.
 		self.seed()
         	self.reset()
 		returnObs=Observation()
@@ -23,10 +27,11 @@ class My_Environment(taxi.TaxiEnv):
 	
 	# (Action) -> Reward_observation_terminal
 	def env_step(self,action):
-		
+		#the taxi can take 6 actions, east,west,north,south,pickup and dropoff.
 		assert len(action.intArray)==1,"Expected 1 integer action."
-		assert action.intArray[0]>=0, "Expected action to be in [0,3]"
-		assert action.intArray[0]<7, "Expected action to be in [0,3]"
+		assert action.intArray[0]>=0, "Expected action to be in [0,5]"
+		assert action.intArray[0]<5, "Expected action to be in [0,5]"
+		#step function is defined in gym/envs/toy_text/discrete.py, the DiscreteEnv class defined in there is inherited by TaxiEnv which MY_Environment inherits.
 		s1, r1, d1, k1=self.step(action.intArray[0])
 		returnRO=Reward_observation_terminal()
 		returnRO.r=r1*1.0
